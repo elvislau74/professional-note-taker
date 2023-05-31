440efb6ee9930710feef70bcddaf1b2413856421
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-05-31 00:09:26 -0400

    comment out routes folder code until bug can be fixed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.use(routes);
+// app.use(routes);
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
