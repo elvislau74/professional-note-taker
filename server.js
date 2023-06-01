4c426870a9de1a40ade0af7a4c7e552b51b22c0a
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-05-31 21:38:34 -0400

    delete unnecessary commented out code

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('fs');
-// const util = require('util');
-// const {readFromFile, writeToFile, readAndAppend} = require('./helpers/fsUtils');
 const routes = require('./routes');
-// const uuid = require('./helpers/uuid');
-// const noteDB = require('./db/db.json');
-
-
 
 const PORT = process.env.PORT || 3001;
 
@@ -27,44 +20,6 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-
-// app.get('/api/notes', (req, res) => {
-//   console.info(`${req.method} request recieved for notes.`);
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-// })
-
-// app.post('/api/notes', (req, res) => {
-//   console.info(`${req.method} request recieved to add a note.`);
-
-//   const {title, text} = req.body;
-
-//   if (req.body) {
-//     const newNote = {
-//       title,
-//       text,
-//       id: uuid()
-//     };
-
-//     readAndAppend(newNote, './db/db.json');
-//     res.json('Note added successfully!')
-//   } else {
-//     res.error('Error in adding note.');
-//   }
-// });
-
-// app.delete('/api/notes/:id', (req, res) => {
-//   const noteToDelete = req.params.id;
-//   const newNotes = [];
-//   if (noteToDelete){
-//     for(notes of noteDB) {
-//       if(notes.id !== noteToDelete){
-//         newNotes.push(notes);
-//       }
-//     }
-//     writeToFile('./db/db.json', newNotes);
-//   };
-// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
